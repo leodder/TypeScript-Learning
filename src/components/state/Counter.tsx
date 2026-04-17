58371aa560df12c1a1af1e38fb226123ddab2113
leodder <a0919989772@gmail.com>
2026-04-17 15:50:38 +0900

    learn: useReducer X Typescript 2

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -3,10 +3,18 @@ import { useReducer } from "react";
 type CounterState = {
     count: number
 }
-type CounterAction = {
-    type: string,
+// type CounterAction = {
+//     type: 'increment' | 'decrement' | 'reset',
+//     payload?: number,
+// }
+type UpdatedAction = {
+    type: 'increment' | 'decrement',
     payload: number,
 }
+type ResetAction = {
+    type: 'reset'
+}
+type CounterAction = UpdatedAction | ResetAction
 
 const initialState = { count: 0 };
 
@@ -16,6 +24,8 @@ function reducer(state: CounterState, action: CounterAction) {
             return { count: state.count + action.payload };
         case 'decrement':
             return { count: state.count - action.payload };
+        case 'reset':
+            return initialState;
         default:
             return state;
     }
@@ -32,6 +42,7 @@ const Counter = () => {
             <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>
                 Decrement 10
             </button>
+            <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
         </>
     )
 }
