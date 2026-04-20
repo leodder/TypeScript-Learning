fdb6204e1f1c380d0cb8faec237dbfc45748f19d
leodder <a0919989772@gmail.com>
2026-04-20 18:04:30 +0900

    learn: useRef X Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Status from "./components/Status";
 // import Box from './components/context/Box';
 import { UserContextProvider } from "./components/context/UserContext";
 import { User } from "./components/context/User";
+import MutableRef from "./components/ref/MutableRef";
 
 function App() {
   const firstName = "Justin";
@@ -26,18 +27,19 @@ function App() {
         <Box /> */}
       <UserContextProvider>
         <User />
-      <Greet name={"Leo"} isLoggedIn={true} />
-      <Person firstName={firstName} lastName={lastName} />
-      <PersonList names={nameList} />
-      <Status status="success" />
-      <Heading>PlaceHolder text</Heading>
-      <Oscars>
-        <Heading>Oscars goes to D.Trump</Heading>
-      </Oscars>
-      <Button handleClick={(event, id) => console.log("123", event, id)} >Click me</Button>
-      <Input value={"Test"} handleChange={(event) => console.log(event)} />
-      <Container styles={{ border: "1px solid black", padding: "10px", position: "relative", top: "10px" }} />
-      {/* </ThemeContextProvider> */}
+        <MutableRef />
+        <Greet name={"Leo"} isLoggedIn={true} />
+        <Person firstName={firstName} lastName={lastName} />
+        <PersonList names={nameList} />
+        <Status status="success" />
+        <Heading>PlaceHolder text</Heading>
+        <Oscars>
+          <Heading>Oscars goes to D.Trump</Heading>
+        </Oscars>
+        <Button handleClick={(event, id) => console.log("123", event, id)} >Click me</Button>
+        <Input value={"Test"} handleChange={(event) => console.log(event)} />
+        <Container styles={{ border: "1px solid black", padding: "10px", position: "relative", top: "10px" }} />
+        {/* </ThemeContextProvider> */}
       </UserContextProvider>
     </>
   )
